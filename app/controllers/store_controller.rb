1254b671268d85519aaee4c3441cc819ca6768c8
Alex Depotovich <alex.github@gmail.com>
2012-02-15 05:55:08 +0300

    Task E with book playtimes

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	
+  	if session[:counter].nil?
+  		session[:counter] = 1
+  	else
+  		session[:counter] += 1
+  	end
+  	@session_counter = session[:counter]
   end
 end
